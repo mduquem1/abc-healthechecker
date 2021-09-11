e57595e6143a351f0ad3aec9d057583e6cb256af
asantaba <as.santamaria@uniandes.edu.co>
2021-09-11 12:42:46 -0500

    add notifier component interaction

--- monitor/modelos/modelos.py
+++ monitor/modelos/modelos.py
@@ -1,5 +1,5 @@
 from flask_mongoengine import MongoEngine
-
+import requests
 
 db = MongoEngine()
 
@@ -12,3 +12,9 @@ class HealthyCheck(db.Document):
             'status': self.status_code,
             'date': self.date
         }
+
+class Notifier():
+    def send_error_notification(self, api, status_code, description):
+        url = 'http://127.0.0.1:5001/notify'
+        myobj = {'api': api,'status_code': status_code,'description': description,'smtp_password': 'here-is-the-password'}
+        requests.post(url, json = myobj)
