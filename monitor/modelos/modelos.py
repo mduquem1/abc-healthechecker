ec317514e5f18279c22f70cc91813a0057209dcb
asantaba <as.santamaria@uniandes.edu.co>
2021-09-11 11:45:21 -0500

    add notifier interaction

--- monitor/modelos/modelos.py
+++ monitor/modelos/modelos.py
@@ -1,5 +1,5 @@
 from flask_mongoengine import MongoEngine
-
+import requests
 
 db = MongoEngine()
 
@@ -12,3 +12,9 @@ class HealthyCheck(db.Document):
             'status': self.status,
             'date': self.date
         }
+
+class Notifier():
+    def send_error_notification(self, api, status_code, description):
+        url = 'http://127.0.0.1:5000/notify'
+        myobj = {'api': api,'status_code': status_code,'description': description,'smtp_password': '6478088dc1f65dcebbb31fc186f47142-fa6e84b7-2009bf3e'}
+        requests.post(url, json = myobj)
--- monitor/vistas/vistas.py
+++ monitor/vistas/vistas.py
@@ -1,3 +1,4 @@
+from monitor.modelos.modelos import Notifier
 from flask import jsonify
 from flask_restful import Resource
 import requests
@@ -11,3 +12,7 @@ class VistaHealthyChecker(Resource):
         nuevo_check = HealthyCheck(status='success', date="{}".format(datetime.utcnow()))
         nuevo_check.save()
         return nuevo_check.to_json()
+
+    def post(self):
+        notifier = Notifier()
+        notifier.send_error_notification('Reportes', 500, 'Error with Reportes clinica API')
